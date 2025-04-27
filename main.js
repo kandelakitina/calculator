0851bd0385d52ad5f19f11d1f67cf401f8f948d5
boticelli <kandelakitina@gmail.com>
2025-04-27 19:37:53 +0300

    Update html classes

---
+++ main.js
@@ -0,0 +1,4 @@
+const calculator = document.querySelector(".calculator");
+const number = calculator.querySelector(".number");
+const operator = calculator.querySelector(".operator");
+const equals = calculator.querySelector(".equals");
